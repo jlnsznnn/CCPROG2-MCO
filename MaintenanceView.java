3cd068acaf0cbf4d7f8fd1a204c3456e9c2c0086
kathleentrc <kathleen_cruz@dlsu.edu.ph>
2023-08-01 20:40:12 +0800

    update maintenance view

--- MaintenanceView.java
+++ MaintenanceView.java
@@ -3,133 +3,110 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MaintenanceView {
-    private JFrame frame;
-    private ImageIcon icon;
-    private JTextField textField;
+public class MaintenanceView extends JFrame implements ActionListener {
+    private JPanel mainPanel;
+    private JPanel buttonPanel;
+    private JPanel contentPanel;
 
     public MaintenanceView() {
-        this.icon = new ImageIcon(getClass().getResource("/Images/VM ICON.png"));
-        this.frame = new JFrame("Customize Your Regular Vending Machine");
+        // Set up the main frame
+        setTitle("Maintenance");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(480, 680);
+        setLocationRelativeTo(null);
 
-        this.frame.setSize(480, 680); // Set the size of the frame
-        this.frame.setIconImage(icon.getImage());
-
-        this.frame.setLocationRelativeTo(null);
-        this.frame.setResizable(false);
-        this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    }
-
-    public void createGUI() {
-        final ImageIcon backgroundImage = new ImageIcon(getClass().getResource("/Images/MAINTENANCE BG.png"));
-        JPanel mainPanel = new JPanel(new BorderLayout()) {
+        // Create the main panel and set layout
+        mainPanel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.drawImage(backgroundImage.getImage(), 0, 0, getWidth(), getHeight(), this);
+                // Load and draw the background image
+                ImageIcon bgImageIcon = new ImageIcon("Images/MAINTENANCE BG.png");
+                Image bgImage = bgImageIcon.getImage();
+                g.drawImage(bgImage, 0, 0, getWidth(), getHeight(), this);
             }
         };
+        mainPanel.setLayout(new BorderLayout());
 
-        frame.getContentPane().add(mainPanel);
+        // Create the button panel on the left
+        buttonPanel = new JPanel();
+        buttonPanel.setOpaque(false); // Make the button panel transparent
+        buttonPanel.setLayout(new GridLayout(8, 1, 10, 10));
 
-        // Left part: buttons panel
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-        mainPanel.add(buttonPanel, BorderLayout.CENTER);
-
-        // Create buttons with specified text and add ActionListener to each button
-        String[] buttonTexts = {
+        // Add buttons to the button panel
+        String[] buttonNames = {
                 "Restock Item", "Set Price", "Collect Money", "Replenish Money",
                 "Starting Inventory", "Ending Inventory", "Transactions", "Exit"
         };
-
-        for (String text : buttonTexts) {
-            JButton button = new JButton(text);
-            button.setAlignmentX(Component.LEFT_ALIGNMENT);
+        for (String name : buttonNames) {
+            JButton button = new JButton(name);
+            button.addActionListener(this);
             buttonPanel.add(button);
-            button.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    // Call the specific method for each button
-                    if (text.equals("Restock Item")) {
-                        restockItem();
-                    } else if (text.equals("Set Price")) {
-                        setPrice();
-                    } else if (text.equals("Collect Money")) {
-                        collectMoney();
-                    } else if (text.equals("Replenish Money")) {
-                        replenishMoney();
-                    } else if (text.equals("Starting Inventory")) {
-                        startingInventory();
-                    } else if (text.equals("Ending Inventory")) {
-                        endingInventory();
-                    } else if (text.equals("Transactions")) {
-                        transactions();
-                    } else if (text.equals("Exit")) {
-                        exit();
-                    }
-                }
-            });
-
-            // Set button background as transparent
-            button.setOpaque(false);
-            button.setContentAreaFilled(false);
-            button.setBorderPainted(false);
         }
 
-        // Right part: additional panel
-        JPanel rightPanel = new JPanel();
-        mainPanel.add(rightPanel, BorderLayout.LINE_END);
+        // Create the content panel on the right
+        contentPanel = new JPanel();
+        contentPanel.setOpaque(false); // Make the content panel transparent
+        contentPanel.setLayout(new BorderLayout());
 
-        // Customize right panel as needed
-        // Add components, set layout, etc.
+        // Add button panel and content panel to the main panel
+        mainPanel.add(buttonPanel, BorderLayout.WEST);
+        mainPanel.add(contentPanel, BorderLayout.CENTER);
 
-        // Uneditable text field at the bottom
-        textField = new JTextField("Displaying text only", 20);
-        textField.setEditable(false);
-        mainPanel.add(textField, BorderLayout.PAGE_END);
+        // Add main panel to the frame
+        add(mainPanel);
 
-        frame.setVisible(true);
+        // Show the GUI
+        setVisible(true);
     }
 
-    // Define the methods for each button functionality
-    public void restockItem() {
-        textField.setText("Restocking Item...");
-    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String buttonName = e.getActionCommand();
+        // Perform specific method based on the clicked button
+        switch (buttonName) {
+            case "Restock Item":
+                // Add code to execute "Restock Item" method
+                // contentPanel.add(new JLabel("Restock Item Method"));
+                break;
+            case "Set Price":
+                // Add code to execute "Set Price" method
+                // contentPanel.add(new JLabel("Set Price Method"));
+                break;
+            case "Collect Money":
+                // Add code to execute "Collect Money" method
+                // contentPanel.add(new JLabel("Collect Money Method"));
+                break;
+            case "Replenish Money":
+                // Add code to execute "Replenish Money" method
+                // contentPanel.add(new JLabel("Replenish Money Method"));
+                break;
+            case "Starting Inventory":
+                // Add code to execute "Starting Inventory" method
+                // contentPanel.add(new JLabel("Starting Inventory Method"));
+                break;
+            case "Ending Inventory":
+                // Add code to execute "Ending Inventory" method
+                // contentPanel.add(new JLabel("Ending Inventory Method"));
+                break;
+            case "Transactions":
+                // Add code to execute "Transactions" method
+                // contentPanel.add(new JLabel("Transactions Method"));
+                break;
+            case "Exit":
+                // Handle exit action
+                System.exit(0);
+                break;
+            default:
+                break;
+        }
 
-    public void setPrice() {
-        textField.setText("Setting Price...");
-    }
-
-    public void collectMoney() {
-        textField.setText("Collecting Money...");
-    }
-
-    public void replenishMoney() {
-        textField.setText("Replenishing Money...");
-    }
-
-    public void startingInventory() {
-        textField.setText("Starting Inventory...");
-    }
-
-    public void endingInventory() {
-        textField.setText("Ending Inventory...");
-    }
-
-    public void transactions() {
-        textField.setText("Viewing Transactions...");
-    }
-
-    public void exit() {
-        textField.setText("Exiting...");
-        System.exit(0);
+        // Refresh the content panel to show the changes
+        contentPanel.revalidate();
+        contentPanel.repaint();
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            MaintenanceView example = new MaintenanceView();
-            example.createGUI();
-        });
+        SwingUtilities.invokeLater(MaintenanceView::new);
     }
 }
